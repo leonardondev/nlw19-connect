9dbf330230d504ae0bb92bbff91d6690ca60c104
Leonardo do Nascimento <leonardo3.nascimento@gmail.com>
2025-02-19 23:54:33 -0300

    dev: redis sorted sets routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,12 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
+
 import { accessInviteLinkRoute } from './routes/access-invite-link.route'
+import { getRankingRoute } from './routes/get-ranking.route'
+import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks.route'
+import { getSubscriberInvitesCountRoute } from './routes/get-subscriber-invites-count.route'
+import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position.route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event.route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -37,6 +42,10 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 app.register(accessInviteLinkRoute)
+app.register(getSubscriberInviteClicksRoute)
+app.register(getSubscriberInvitesCountRoute)
+app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app
   .listen({
